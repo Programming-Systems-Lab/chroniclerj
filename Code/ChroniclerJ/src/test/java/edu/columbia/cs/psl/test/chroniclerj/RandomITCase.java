6dc16122a6281387230a3167f43469bf6d2bc522
Jonathan Bell <jbell@cs.columbia.edu>
2016-10-17 20:49:14 -0400

    Testing setup

---
+++ Code/ChroniclerJ/src/test/java/edu/columbia/cs/psl/test/chroniclerj/RandomITCase.java
@@ -0,0 +1,13 @@
+package edu.columbia.cs.psl.test.chroniclerj;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RandomITCase {
+
+	@Test
+	public void testRandom() throws Exception {
+		System.out.println(Math.random());
+	}
+}
